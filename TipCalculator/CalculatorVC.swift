f524fb8b031040d33272cd97722891a9030f9147
Yianni1980 <ioagian1980@gmail.com>
2023-07-06 12:21:56 +0300

    Compute resuls in view Model

--- TipCalculator/CalculatorVC.swift
+++ TipCalculator/CalculatorVC.swift
@@ -56,7 +56,9 @@ class CalculatorVC: UIViewController {
             splitPublisher: splitInputView.valuePublisher)
         let output = vm.transform(input: input)
         
-       
+        output.updateViewPublisher.sink { result in
+            print(result)
+        }.store(in: &cancellables)
         
     }
     
